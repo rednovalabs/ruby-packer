9225869124365a3f853f3dbb0cb9f3a26662a2b1
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-11-28 09:46:57 +0200

    Update build / deploy scripts

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,17 +5,27 @@ set -e
 
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
   curl -sL https://github.com/aktau/github-release/releases/download/v0.7.2/darwin-amd64-github-release.tar.bz2 | tar -xjO > /tmp/github-release
-  UPLOAD_OS="darwin"
+  brew install openssl || brew upgrade openssl || true
+  curl -sL https://curl.haxx.se/ca/cacert.pem > /usr/local/etc/openssl/cacert.pem || true
+  OPENSSL_DIR="/usr/local/etc/openssl"
+  TEMP_DIR="$(mktemp -d)"
 else
   curl -sL https://github.com/aktau/github-release/releases/download/v0.7.2/linux-amd64-github-release.tar.bz2 | tar -xjO > /tmp/github-release
-  UPLOAD_OS="linux"
+  apt-get install -y -q openssl || true
+  update-ca-certificates || true
+  OPENSSLDIR="/etc/ssl"
+  TEMP_DIR="$(mktemp -d --tmpdir=/root .rubyc-build.XXXXXX)"
 fi
 
-
-chmod +x /tmp/github-release
-
-ruby -Ilib bin/rubyc bin/rubyc -o rubyc --clean-tmpdir
-
+ruby -Ilib bin/rubyc bin/rubyc \
+  --openssl-dir=${OPENSSLDIR} \
+  --tmpdir=${TEMP_DIR} \
+  --clean-tmpdir \
+  --ignore-file=.git \
+  --ignore-file=.travis.yml \
+  --ignore-file=.travis/deploy.sh \
+  --ignore-file=.travis/osx_deps.sh \
+  -o rubyc
 
 strip rubyc || true
 
@@ -23,9 +33,10 @@ strip rubyc || true
 
 gzip rubyc
 
+chmod +x /tmp/github-release
 GITHUB_TOKEN="${GH_TOKEN}" /tmp/github-release upload \
     --user kke \
     --repo ruby-packer \
     --tag $TRAVIS_TAG \
-    --name "rubyc-${TRAVIS_TAG#v}-${UPLOAD_OS}-amd64.gz" \
+    --name "rubyc-${TRAVIS_TAG}-${TRAVIS_OS_NAME}-amd64.gz" \
     --file ./rubyc.gz
