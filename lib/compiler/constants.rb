4627886c73e216c28543a86b94ffde2f68b61f1a
Kimmo Lehto <kimmo.lehto@kontena.io>
2019-01-04 11:42:01 +0200

    Change rubyc version to 0.6.0

--- lib/compiler/constants.rb
+++ lib/compiler/constants.rb
@@ -1,10 +1,10 @@
 # Copyright (c) 2017 Minqi Pan <pmq2001@gmail.com>
-# 
+#
 # This file is part of Ruby Compiler, distributed under the MIT License
 # For full terms see the included LICENSE file
 
 class Compiler
-  VERSION = '0.5.0'
+  VERSION = '0.6.0'
   PRJ_ROOT = File.expand_path('../../..', __FILE__)
   VENDOR_DIR = File.expand_path('vendor', PRJ_ROOT)
   MEMFS = '/__enclose_io_memfs__'
